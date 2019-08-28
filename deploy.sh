fbed82a6ef116c49b7af558755dd07dd21bb360e
Renjith <renjith.george@msisoft.in>
2019-08-28 18:07:32 +0530

    update travis file and encrypted google credentails

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+docker build -t rvettom/multi-client:latest -t rvettom/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t rvettom/multi-server:latest -t rvettom/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rvettom/multi-worker:latest -t rvettom/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push rvettom/multi-client:latest
+docker push rvettom/multi-server:latest
+docker push rvettom/multi-worker:latest
+
+docker push rvettom/multi-client:$SHA
+docker push rvettom/multi-server:$SHA
+docker push rvettom/multi-worker:$SHA
+
+kubectl set image deployments/server-deployment server=rvettom/multi-server$SHA
+kubectl set image deployments/client-deployment client=rvettom/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=rvettom/multi-worker:$SHA
